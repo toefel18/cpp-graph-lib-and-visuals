9f67a8b34620dde552834da25ddc8d1dabad71bb
toefel18@gmail.com <toefel18@gmail.com@edab2469-ce68-9e0b-be63-8e78985c5434>
2012-04-04 17:12:48 +0000

    added files for gui project, project should now be migrated nicely

---
+++ GraphAndVisuals/visualgraph/guivertex.h
@@ -0,0 +1,49 @@
+#ifndef GUIVERTEX_H
+#define GUIVERTEX_H
+
+#include <QtGui/QGraphicsEllipseItem>
+#include <vector>
+#include <QtGui/QColor>
+
+class GuiEdge;
+
+/*!
+* \brief Graphical vertex that can be connected by edges
+*
+* \author Christophe Hesters 
+* \date	6-1-2011 
+* \note tested on VC++ 10.0
+*/
+class GuiVertex : public QGraphicsItem
+{
+
+public:
+	GuiVertex();
+	~GuiVertex();
+
+	QColor getColor(){return color;}
+
+	virtual QRectF boundingRect() const {return vertexBoundingRect;}
+	virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+	void setColor(QColor color){this->color = color; update(vertexBoundingRect);}
+	void setTextColor(QColor color){this->textColor = color; update(vertexBoundingRect);}
+	void setText(QString text){this->innerText = text; update(vertexBoundingRect);}
+
+	void addEdge(GuiEdge* edge);
+	
+protected:
+	QColor color;
+	QColor textColor;
+	QRectF vertexBoundingRect;
+	
+	QString innerText;
+	qreal innerTextHeight;
+
+	virtual QVariant itemChange(GraphicsItemChange change, const QVariant &value);
+
+	typedef std::vector<GuiEdge*> EdgeList;
+	EdgeList edges;
+};
+
+#endif //GUIVERTEX_H
